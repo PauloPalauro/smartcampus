3f48096746e42497811a5c91bbec71df16abf3aa
PauloPalauro <paulopalauro@gmail.com>
2024-05-10 18:23:33 -0300

    Modify querys parameters and JSON

--- influx.py
+++ influx.py
@@ -17,54 +17,58 @@ token = "Dmd7pZotErhWPDkf0mJEyVffDVJAAmFreaf3xfM7edm-gt12xXdlcxGv8E8MUrSK31o4zLx
 url = "https://us-east-1-1.aws.cloud2.influxdata.com/"
 client = InfluxDBClient(url=url, token=token, org=org)
 
-# Define um End-Point da API para recuperar dados do sensor WaterTankLavel com base no seu nome;
-@app.route('/api/data/WaterTankLavel/<nodename>', methods=['GET'])
-def get_WaterTankLavel_by_nodename(nodename):
-    # Query InfluxDB para recuperar dados do hidrômetro com base no nome --> https://docs.influxdata.com/influxdb/v2/query-data/flux/;
-    query = f'''
-        from(bucket:"{bucket}") 
-            |> range(start: -1h)
-            |> filter(fn: (r) => r._measurement == "WaterTankLavel")
-            |> filter(fn: (r) => r.nodeName == "{nodename}")
-            |> filter(fn: (r) => r._field == "data_boardVoltage" or r._field == "data_distance")
-    '''
-    # "client.query_api()" para obter uma instância da API de consulta do InfluxDB.  "query(query)" para enviar a consulta "query" ao banco de dados e receber os resultados. O resultado da consulta é armazenado na variável "result".
-    result = client.query_api().query(query)
 
-    # inicializando um dicionário chamado data que será usado para armazenar os dados recuperados da consulta ao banco de dados
-    data = {'nodeName': nodename, 'time': [], 'data_boardVoltage': [], 'data_distance': []}
+@app.route('/api/data/WaterTankLavel', methods=['GET'])
+def get_WaterTankLavel():
+    data_by_nodename = {}
+    for nodename in range(1, 9):  
+        query = f'''
+            from(bucket:"{bucket}") 
+                |> range(start: -1h)
+                |> filter(fn: (r) => r._measurement == "WaterTankLavel")
+                |> filter(fn: (r) => r.nodeName == "WaterTankLavel_{nodename}")
+                |> filter(fn: (r) => r._field == "data_distance")
+        '''
+        result = client.query_api().query(query)
+        nodename_data = {'data_distance': [], 'timestamp': []}
 
-    # Iteramos sobre os resultados da consulta (result). Retornar múltiplas tabelas de resultados, estamos percorrendo cada tabela (table) e, em seguida, iterando sobre os registros (record) dentro de cada tabela.
-    for table in result:
-        for record in table.records:
-            time = record.get_time().astimezone(pytz.timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S') # convertemos este tempo para o fuso horário de São Paulo
-            data['time'].append(time)
-            field = record.get_field() # Obtém o nome do campo
-            value = record.get_value() # Obtém o valor do campo
-            data[field].append(value)
-    return jsonify(data)  # Retorna os dados em formato JSON 
+        for table in result:
+            for record in table.records:
+                time = record.get_time().astimezone(pytz.timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S')
+                nodename_data['timestamp'].append(time)
+                value = record.get_value()
+                nodename_data['data_distance'].append(value)
+        data_by_nodename[f'WaterTankLavel_{nodename}'] = nodename_data
+
+    return jsonify(data_by_nodename)
 
 
-# Mesmo processo acima só que com hidrometer
-@app.route('/api/data/Hidrometer/<nodename>', methods=['GET'])
-def get_Hidrometer_by_nodename(nodename):
-    query = f'''
-        from(bucket:"{bucket}")
-            |> range(start: -1h)
-            |> filter(fn: (r) => r._measurement == "Hidrometer")
-            |> filter(fn: (r) => r.nodeName == "{nodename}")
-            |> filter(fn: (r) => r._field == "data_boardVoltage" or r._field == "data_counter")
-    '''
-    result = client.query_api().query(query)
-    data = {'nodeName': nodename,'time': [], 'data_boardVoltage': [], 'data_counter': []}
-    for table in result:
-        for record in table.records:
-            time = record.get_time().astimezone(pytz.timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S')
-            data['time'].append(time)
-            field = record.get_field()
-            value = record.get_value()
-            data[field].append(value)
-    return jsonify(data)
+
+
+@app.route('/api/data/Hidrometer', methods=['GET'])
+def get_Hidrometer():
+    data_by_nodename = {}
+    for nodename in range(1, 9):  
+        query = f'''
+            from(bucket:"{bucket}") 
+                |> range(start: -1h)
+                |> filter(fn: (r) => r._measurement == "Hidrometer")
+                |> filter(fn: (r) => r.nodeName == "Hidrometer_{nodename}")
+                |> filter(fn: (r) => r._field == "data_counter")
+        '''
+        result = client.query_api().query(query)
+        nodename_data = {'data_counter': [], 'timestamp': []}
+
+        for table in result:
+            for record in table.records:
+                time = record.get_time().astimezone(pytz.timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S')
+                nodename_data['timestamp'].append(time)
+                value = record.get_value()
+                nodename_data['data_counter'].append(value)
+        data_by_nodename[f'Hidrometer_{nodename}'] = nodename_data
+    return jsonify(data_by_nodename)
+
+
 
 
 @app.route('/api/data/ArtesianWell', methods=['GET'])
@@ -74,18 +78,18 @@ def get_ArtesianWell():
             |> range(start: -1h)
             |> filter(fn: (r) => r._measurement == "ArtesianWell")
             |> filter(fn: (r) => r.nodeName == "ArtesianWell_1")
-            |> filter(fn: (r) => r._field == "data_boardVoltage" or r._field == "data_pressure_0" or r._field == "data_pressure_1")
+            |> filter(fn: (r) => r._field == "data_pressure_0" or r._field == "data_pressure_1")
     '''
     result = client.query_api().query(query)
-    data = {'nodeName': "ArtesianWell_1", 'time': [], 'data_boardVoltage': [], 'data_pressure_0': [], 'data_pressure_1': []}
+    data = {'data_pressure_0': [], 'data_pressure_1': [], 'timestamp': []}
     for table in result:
         for record in table.records:
             time = record.get_time().astimezone(pytz.timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S')
-            data['time'].append(time)
+            data['timestamp'].append(time)
             field = record.get_field()
             value = record.get_value()
             data[field].append(value)
-    return jsonify(data)
+    return jsonify({"ArtesianWell": data})
 
 
 # Inicia o servidor Flask em modo de depuração
